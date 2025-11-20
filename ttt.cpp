30d5524f05167361c80475bd9801aea457bb475b
Hasnain <hasnain365syed@gmail.com>
2025-11-19 20:23:19 -0500

    fixed more bugs

--- ttt.cpp
+++ ttt.cpp
@@ -7,19 +7,23 @@ using namespace std;
 // ttt
 
 
+// All the winning conditions
+// Initialized for the check_win() function
+const int win[8][3] = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8},{2,4,6}};
+
 // Set up a game state with an array of integers
 // The even-number-th item on the array store the location that player 1 placed a piece
 // The odd-number-th item on the array store the location that player 2 placed a piece
 class gameState{
     public:
-        int gameLength = 1; // Length of current game (length of game array)
+        int gameLength = 0; // Length of current game (length of game array)
         int gameState[9]; // Current game in a string
         char player1Choice; // 'x' or 'o', player 1 plays first
         char player2Choice; // 'o' or 'x', player 2 plays second
         bool turn = true; // true for player 1's turn, false for player 2's turn
 };
 
-void pieceAssignment(gameState g){
+void pieceAssignment(gameState& g){
     int choice;
     cout << "Which piece does player 1 want?\n";
     cout << "Please choose from one of the following.\n";
@@ -54,7 +58,7 @@ void print_input_board(){
     cout << top + "-+-+-\n" + middle + "-+-+-\n" + bottom;
 }
 
-string set_up_board(gameState g){
+string set_up_board(gameState& g){
     string top = " | | \n";
     string middle = " | | \n";
     string bottom = " | | \n";
@@ -64,46 +68,32 @@ string set_up_board(gameState g){
             case 0:
                 // Use the remainder after division with 3 to check location
                 // Turn checking with (i % 2). If even, player 1. Else, player 2.
-                top[2 * (g.gameState[i] % 3)] = (i % 2 == 0) ? g.player1Choice : g.player2Choice;
+                top[2 * (g.gameState[i] % 3)] = (g.turn) ? g.player1Choice : g.player2Choice;
+                break;
             case 1:
-                middle[2 * (g.gameState[i] % 3)] = (i % 2 == 0) ? g.player1Choice : g.player2Choice;
+                middle[2 * (g.gameState[i] % 3)] = (g.turn) ? g.player1Choice : g.player2Choice;
+                break;
             case 2:
-                bottom[2 * (g.gameState[i] % 3)] = (i % 2 == 0) ? g.player1Choice : g.player2Choice;
+                bottom[2 * (g.gameState[i] % 3)] = (g.turn) ? g.player1Choice : g.player2Choice;
+                break;
         }
     }
 
     return top + "-+-+-\n" + middle + "-+-+-\n" + bottom;
 }
 
-bool check_win(gameState g){
+bool check_win(gameState& g){
     // Horizontal => 012,345,678
     // Vertical => 036,147,258
     // Diagonal => 048,246
 
-    // All the winning conditions
-    int win[8][3] = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8},{2,4,6}};
-
-    int player[5]; // Only need to check once for the player that just played
+    int player[9]; // Only need to check once for the player that just played
     int len;
 
     // Turn resets after get_input() is called, so it would be player 2's turn when checking if player 1 won
     // Player 1 has one more piece than player 2
     // Odd number of pieces on the board
     if (!g.turn){
-        len = g.gameLength / 2 + 1;
-        if (len < 3){
-            return false;
-        }
-        for (int i = 0; i < g.gameLength; i++){
-            if (i % 2 == 0){
-                player[i/2] = g.gameState[i];
-                // Player 1 occupies all the even-number-th int
-            }
-        }
-    }
-    // Even number of pieces on the board
-    // Player 1 and player 2 have the same amount of pieces
-    else{
         len = g.gameLength / 2;
         if (len < 3){
             return false;
@@ -115,30 +105,44 @@ bool check_win(gameState g){
             }
         }
     }
+    // Even number of pieces on the board
+    // Player 1 and player 2 have the same amount of pieces
+    else{
+        len = g.gameLength / 2 + 1;
+        if (len < 3){
+            return false;
+        }
+        for (int i = 0; i < g.gameLength; i++){
+            if (i % 2 == 0){
+                player[i/2] = g.gameState[i];
+                // Player 1 occupies all the even-number-th int
+            }
+        }
+    }
 
     // check
     int connect = 0;
     // Looping through all the cases
     for (int i = 0; i < 8; i++){
         // Looping through the individual cell in every case
+        connect = 0; // Reset connect to 0 when checking every case
         for (int j = 0; j < 3; j++){
             // Looping through all the currently occupied cell
-            connect = 0; // Reset connect to 0 when checking every case
             for (int k = 0; k < len; k++){
                 if (player[k] == win[i][j]){
                     connect++;
                 }
             }
-            if (connect == 3){
-                return true; // When there are three connecting pieces, return true
-            }
+        }
+        if (connect == 3){
+            return true; // When there are three connecting pieces, return true
         }
     }
 
     return false;
 }
 
-void get_input(gameState g){
+void get_input(gameState& g){
     int output;
     cout << "Type an integer from 0 to 8:";
     while (!(cin >> output)){
@@ -152,6 +156,13 @@ void get_input(gameState g){
         return get_input(g);
     }
 
+    for (int i = 0; i < g.gameLength; i++){
+        if (g.gameState[i] == output){ /* reject */ 
+            cout << "That square is occupied! Try again!\n";
+            return get_input(g);
+        }
+    }
+
     g.gameState[g.gameLength-1] = output;
     g.gameLength++;
     g.turn = !g.turn;
@@ -160,6 +171,8 @@ void get_input(gameState g){
 int main(){
     bool over = false;
     gameState ttt;
+    // Initialize gameState and fill the entire array with 0s
+    fill(begin(ttt.gameState), end(ttt.gameState), 0);
 
     cout << "####################### \n";
     cout << "Welcome to Tic Tac Toe! \n";
@@ -170,7 +183,7 @@ int main(){
     cout << "\nYour game starts now!\n\n";
 
     while (!over){
-        if (ttt.gameLength == 9){
+        if (ttt.gameLength == 9 && !check_win(ttt)){
             over = true;
         }
 
@@ -202,12 +215,11 @@ int main(){
         }
     }
 
+    if (ttt.gameLength == 9 && !check_win(ttt)){
     cout << "The game was drawn. Thanks for playing!\n";
     cout << "Final Board: \n";
     cout << set_up_board(ttt);
+    }
     
     return 0;
 }
-
-
-
